fc253164950e4d1f3e2135111cd137e14bd170b0
Adit0507 <adi05072002@gmail.com>
2022-10-30 12:58:23 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
+import { useEffect } from "react";
+import { Home } from "./components/Home";
+import { Info } from "./components/Info";
+import { Details } from "./components/Details";
+import { Episodes } from "./components/Episodes";
+import { Videos } from "./components/Videos";
+import { Footer } from "./components/Footer";
+import ScrollReveal from "scrollreveal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const sc = ScrollReveal({
+      origin: "bottom",
+      distance: "80px",
+      duration: 4000,
+      reset: false,
+    });
+    sc.reveal(
+      `
+        #home,
+        #info,
+        #episodes,
+        #details,
+        #videos,
+        footer
+      `,
+      {
+        opacity: 0,
+        interval: 500,
+      }
+    );
+  }, []);
 
   return (
-    <div className="App">
-      <h1>UYUIGB</h1>
+    <div>
+      <Home />
+      <Info />
+      <Episodes />
+      <Videos />
+      <Details />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
