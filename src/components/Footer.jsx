55d0a074de3d7af29df66c81b9354f7c2924ceed
Adit0507 <adi05072002@gmail.com>
2022-10-30 12:57:54 +0530

    Finished the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,57 @@ import styled from "styled-components";
 import { fontLightColor } from "../utils/colors";
 
 export const Footer = () => {
-  return <Container></Container>;
+  return (
+    <Container className="defaultMargin">
+      <ul>
+        <li>
+          <a href="#">FAQ</a>
+        </li>
+        <li>
+          <a href="#">Help Centre</a>
+        </li>
+        <li>
+          <a href="#">Account</a>
+        </li>
+        <li>
+          <a href="#">Media Centre</a>
+        </li>
+        <li>
+          <a href="#">Investor Relations</a>
+        </li>
+        <li>
+          <a href="#">Jobs</a>
+        </li>
+        <li>
+          <a href="#">Ways to Watch</a>
+        </li>
+        <li>
+          <a href="#">Terms of Use</a>
+        </li>
+        <li>
+          <a href="#">Privacy</a>
+        </li>
+        <li>
+          <a href="#">Cookie Preferences</a>
+        </li>
+        <li>
+          <a href="#">Corporate Information</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+        <li>
+          <a href="#">Speed Test</a>
+        </li>
+        <li>
+          <a href="#">Legal Notices</a>
+        </li>
+        <li>
+          <a href="#">Only on Netflix</a>
+        </li>
+      </ul>
+    </Container>
+  );
 };
 
 const Container = styled.footer`
