88ec0a2be962af7ba0a2f96a09d224591e06f67d
Adit0507 <adi05072002@gmail.com>
2022-10-30 12:42:24 +0530

    Finished the Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { fontLightColor } from "../utils/colors";
+
+export const Footer = () => {
+  return <div>Footer</div>;
+};
+
+const Container = styled.footer`
+  display: flex;
+  justify-content: center;
+  border-top: 2px solid ${fontLightColor};
+  margin: 0 5rem;
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    column-gap: 18rem;
+    row-gap: 2rem;
+    list-style-type: none;
+    li {
+      a {
+        text-transform: uppercase;
+        color: ${fontLightColor};
+        text-decoration: none;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: white;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    margin: 0 5vw;
+    ul {
+      grid-template-columns: repeat(2, 1fr);
+      column-gap: 1rem;
+    }
+  }
+`;
